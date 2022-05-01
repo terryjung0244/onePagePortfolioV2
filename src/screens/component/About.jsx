9b340e811b963d7865941eb522f80e29754bcc1c
terryjung0244 <terryjung0244@gmail.com>
2022-04-30 20:38:52 -0400

    About style Responsive Updated

--- src/screens/component/About.jsx
+++ src/screens/component/About.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './../Style/about.css'
+import bgImage from './../../resources/image/aboutMe.jpg'
 
 const About = () => {
   return (
-    <div>
-      A
+    <div className='aboutMainCon'>
+      <div className='aboutLeftSubCon'>
+        <div className='aboutTitleCon'>
+          About Me
+        </div>
+        <div className='aboutDescCon'>
+          Experience with all phases of the software life-cyle. Passion for learning new technologies. Capable of working within a team envirnment and independently. Ability to plan and time schedule, work under strict time pressure and adapt quickly when needed.
+        </div>
+        <div className='aboutTalkCon'>
+          Let's Talk
+        </div>
+      </div>
+      <div className='aboutSecondSubCon'>
+        asdasds
+           {/* <img className='backGroundPictureCon' src={bgImage} alt='bgImageError.jpg'/> */}
+      </div>
     </div>
   )
 }

69efda3a6aaf285df7c09a524ac20d48635d2c65
terryjung0244 <terryjung0244@gmail.com>
2022-05-01 19:39:38 -0400

    Skill style Responsive Updated

--- src/screens/component/Skill.jsx
+++ src/screens/component/Skill.jsx
@@ -4,7 +4,25 @@ import './../Style/skill.css'
 const Skill = () => {
   return (
     <div className='skillMainCon'>
-      S
+      <div className='skillTopCon'>
+        <div className='skillTitleCon'>
+          SkillSet
+        </div>
+        <div className='skillDescCon'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+        </div>
+      </div>
+      <div className='skillBottomCon'>
+        <div className='skillDevCon'>
+          Development Skills
+        </div>
+        <div className='skillProjectCon'>
+          Project Skills
+        </div>
+        <div className='skillPersonalCon'>
+          Personal Skills
+        </div>
+      </div>
     </div>
   )
 }
